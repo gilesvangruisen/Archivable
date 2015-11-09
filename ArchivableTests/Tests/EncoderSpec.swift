8db05053cea682423f1369c840a5f06b9bcb75a1
Giles Van Gruisen <gilesvangruisen@me.com>
2015-11-09 08:10:15 -0500

    simplify spec, add test for encodedData

--- ArchivableTests/Tests/EncoderSpec.swift
+++ ArchivableTests/Tests/EncoderSpec.swift
@@ -25,43 +25,50 @@ func tailDecode(data: NSData?, _ keys: [String]) -> AnyObject? {
 class EncoderSpec: QuickSpec {
     override func spec() {
         describe("Encoder") {
-            describe("encoding an ArchivableStandardType value") {
-                it("should encode directly on archiver successfully") {
-                    let original = "some string"
-                    let encoder = Encoder().encode(original, forKey: "some key")
-                    let value = tailDecode(encoder.encodedData(), ["some key"])
+            it("should encode an ArchivableStandardType value") {
+                let original = "some string"
+                let encoder = Encoder().encode(original, forKey: "some key")
+                let value = decode(encoder.encodedData(), "some key")
 
-                    expect(value).toNot(beNil())
-                    expect(value as? String).to(equal(original))
+                expect(value).toNot(beNil())
+                expect(value as? String).to(equal(original))
+            }
+
+            it("should encode an Archivable value") {
+                let original = Place(city: "Newport", state: "RI")
+                let encoder = Encoder().encode(original, forKey: "root")
+                let city = tailDecode(encoder.encodedData(), ["root", "city"]) as? String
+
+                expect(city).toNot(beNil())
+                expect(city).to(equal(original.city))
+            }
+
+            it("should recursively encode an Archivable value with a stored Archivable property") {
+                let originalHome = Place(city: "Newport", state: "RI")
+                let originalPerson = Person(name: "Giles", age: 21, home: originalHome)
+
+                let encoder = Encoder().encode(originalPerson, forKey: "root")
+                let city = tailDecode(encoder.encodedData(), ["root", "home", "city"]) as? String
+                expect(city).toNot(beNil())
+                expect(city).to(equal(originalPerson.home.city))
+            }
+
+
+            describe("data returned from encodedData()") {
+                it("should match manually encoded data") {
+                    let place = Place(city: "Boston", state: "MA")
+
+                    let autoEncoder = Encoder().encode(place, forKey: "root")
+                    let manualEncoder = Encoder().encode(place, forKey: "root")
+
+                    let autoData = autoEncoder.encodedData()
+                    manualEncoder.archiver.finishEncoding()
+                    let manualData = NSData(data: manualEncoder.data)
+
+                    expect(manualData).to(equal(autoData))
                 }
             }
 
-            describe("encoding an Archivable value") {
-                context("when the value is a struct") {
-
-                    it("should encode stored properties on archiver") {
-                        let original = Place(city: "Newport", state: "RI")
-                        let encoder = Encoder().encode(original, forKey: "root")
-                        let city = tailDecode(encoder.encodedData(), ["root", "city"]) as? String
-
-                        expect(city).toNot(beNil())
-                        expect(city).to(equal(original.city))
-                    }
-                }
-
-                context("when the value has multiple layers of stored properties") {
-                    it("should recursively encode its encoded properties on archiver") {
-                        let originalHome = Place(city: "Newport", state: "RI")
-                        let originalPerson = Person(name: "Giles", age: 21, home: originalHome)
-
-                        let encoder = Encoder().encode(originalPerson, forKey: "root")
-                        let city = tailDecode(encoder.encodedData(), ["root", "home", "city"]) as? String
-
-                        expect(city).toNot(beNil())
-                        expect(city).to(equal(originalPerson.home.city))
-                    }
-                }
-            }
         }
     }
 }
